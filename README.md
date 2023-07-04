941544fcceac179428749e509b895a5787a146f0
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-04 13:26:44 -0400

    Update

--- README.md
+++ README.md
@@ -5,16 +5,32 @@
 
 ### 1 - INTRODUÇÃO
 
-- Laragon 
+- Laragon
+    - Site: https://laragon.org/download/index.html
 - Codeigniter 4
     - Site: https://www.codeigniter.com/
     - Downloads: https://www.codeigniter.com/download
 - PHP 
 - MySQL 
 - Postman
+    - Site: https://www.postman.com/
 - HeidiSQL
-
+    - Site: https://www.heidisql.com/
+- Bootstrap
+- Bootswatch
+    - Site: https://bootswatch.com/
+    - Theme Slate: https://bootswatch.com/slate/
 **CRUD**
+
+ - env
+
+- Criar a Main
+```
+php spark make:controller Main
+```
+
+- Exemplo:
+    - code-app_01
 
 
 
--- code-app_01/app/Config/Routes.php
+++ code-app_01/app/Config/Routes.php
@@ -11,7 +11,7 @@ $routes = Services::routes();
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Main');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -29,7 +29,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Main::index');
 
 /*
  * --------------------------------------------------------------------
