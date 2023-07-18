8e54b4c58bd38b8936aeb60398826308c50669a6
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-18 09:06:39 -0400

    CONTINUAÇÃO DA PÁGINA INCIAL DA APLICAÇÃO

--- code-app_06/app/Views/main.php
+++ code-app_06/app/Views/main.php
@@ -9,16 +9,69 @@
             <div class="d-flex align-items-center">
                 <label for="search" class="me-3"><strong>Pesquisar</strong></label>
                 <input type="text" class="form-control form-control-sm w-50 me-3" id="search" name="search" placeholder="Pesquisar">
-                <button type="submit" class="btn btn-primary"><i class="fa-solid fa-magnifying-glass me-3"></i> Pesquisar</button>
+                <button type="submit" class="btn btn-primary d-flex align-items-center"><i class="fa-solid fa-magnifying-glass me-3"></i> Pesquisar</button>
+            
+                <span class="mx-3"></span>
+
+                <label for="select_project" class="me-3"><strong>Projeto</strong></label>
+                <select class="form-select form-select-sm" name="select_project" id="select_project">
+                    <option selected>Todas as Tabelas</option>
+                    <option value="1">Tabela 1</option>
+                    <option value="2">Tabela 2</option>
+                    <option value="3">Tabela 3</option>
+                </select>
             </div>
         </div>
         <div class="col-md-3 col-12 text-end">
             <a href="#" class="btn btn-primary"><i class="fa-solid fa-plus me-3"></i> Adicionar</a></a>
         </div>
     </div>
-    
-    
 
+    <!-- Tabela dos resultados -->
+    <div class="row mt-5">
+        <div class="col">
+            <table class="table table-striped" id="table-results">
+                <thead>
+                    <tr>
+                        <th>Projeto</th>
+                        <th>Query</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- ciclo para apresentar as queries -->
+                    <tr>
+                        <td>Projeto 1</td>
+                        <td>Query 1</td>
+                        <td>
+                            <a href="#" class="btn btn-primary"><i class="fa-solid fa-pen-to-square me-3"></i> Editar</a>
+                            <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash me-3"></i> Excluir</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Projeto 2</td>
+                        <td>Query 2</td>
+                        <td>
+                            <a href="#" class="btn btn-primary"><i class="fa-solid fa-pen-to-square me-3"></i> Editar</a>
+                            <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash me-3"></i> Excluir</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Projeto 3</td>
+                        <td>Query 3</td>
+                        <td>
+                            <a href="#" class="btn btn-primary"><i class="fa-solid fa-pen-to-square me-3"></i> Editar</a>
+                            <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash me-3"></i> Excluir</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+    <p class="mt-5">Não foram encontrados resultados</p> 
 
 
     <?= form_close() ?>
