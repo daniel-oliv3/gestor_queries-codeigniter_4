cc222f8b9c5b11f978f7eeee3e5a2c6b60e8a43c
Daniel Oliveira <danielsaapup3@gmail.com>
2023-07-24 10:11:05 -0400

    FORMULÁRIO PARA NOVA QUERY

---
+++ code-app_08/app/Views/main.php
@@ -0,0 +1,76 @@
+<?= $this->extend('layouts/main_layout') ?>
+<?= $this->section('content') ?>
+
+<nav class="container-fluid p-4">
+    <?= form_open('main/search_submit') ?>
+
+    <div class="row">
+        <div class="col-md-9 col-12">
+            <div class="d-flex align-items-center">
+                <label for="search" class="me-3"><strong>Pesquisar</strong></label>
+                <input type="text" class="form-control form-control-sm w-50 me-3" id="search" name="search" placeholder="Pesquisar">
+                <button type="submit" class="btn btn-primary d-flex align-items-center"><i class="fa-solid fa-magnifying-glass me-3"></i> Pesquisar</button>
+            
+                <span class="mx-3"></span>
+
+                <label for="select_project" class="me-3"><strong>Projeto</strong></label>
+                <select class="form-select form-select-sm" name="select_project" id="select_project">
+                    <option selected>Todas as Tabelas</option>
+                    <option value="1">Tabela 1</option>
+                    <option value="2">Tabela 2</option>
+                    <option value="3">Tabela 3</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-3 col-12 text-end">
+            <a href="<?= site_url('new_query') ?>" class="btn btn-primary"><i class="fa-solid fa-plus me-3"></i> Adicionar</a>
+        </div>
+    </div>
+
+    
+    
+    <?= form_close() ?>
+</nav>
+
+
+<!-- Tabela dos resultados -->
+<div class="container-fluid">
+    <div class="row mt-5">
+        <div class="col">
+            <table class="table table-striped" id="table-results">
+                <thead>
+                    <tr>
+                        <th>Projeto</th>
+                        <th>Query</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- ciclo para apresentar as queries -->
+                    <?php for($i = 0; $i < 100; $i++) : ?>
+                    <tr>
+                        <td>Projeto 1</td>
+                        <td>Query 1</td>
+                        <td>
+                            <a href="#" class="btn btn-primary"><i class="fa-solid fa-pen-to-square me-3"></i> Editar</a>
+                            <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash me-3"></i> Excluir</a>
+                        </td>
+                    </tr>
+
+                    <?php endfor; ?>
+                    
+    
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <p class="mt-5 text-center">Não foram encontrados resultados</p> 
+</div>
+
+
+
+
+
+
+<?= $this->endSection() ?>
---
+++ code-app_08/public/assets/js/app.js
@@ -0,0 +1,31 @@
+/*  App.js  */
+
+$(document).ready(function () {
+  // transform table-results into datatable
+  $('#table-results').DataTable({
+    paging: true,
+    ordering: true,
+    pageLength: 10,
+    pagingType: "full_numbers",
+
+    //datatable in portuguese language
+    language: {
+      decimal: "",
+      emptyTable: "Nenhum registro encontrado",
+      info: "Mostrando _START_ ate _END_ de _TOTAL_ registros",
+      infoEmpty: "Mostrando 0 até 0 de 0 registros",
+      infoFiltered: "(Filtrados de _MAX_ registros)",
+      infoPostFix: "",
+      thousands: ".",
+      lengthMenu: "_MENU_ resultados por pagina",
+      search: "Pesquisar:",
+      zeroRecords: "Nenhum registro encontrado",
+      paginate: {
+        first: "Primeiro",
+        last: "Ultimo",
+        next: "Proximo",
+        previous: "Anterior",
+      },
+    },
+  });
+});
